5f4899544afd8ff15fc726f76243bf5e47647c4c
Chan <90392240+cham9994@users.noreply.github.com>
2021-10-16 15:45:26 +0900

    JS:  Project filtering

--- main.js
+++ main.js
@@ -52,6 +52,29 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home')
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work__projects')
+const projetcs = document.querySelectorAll('.project')
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+  
+  setTimeout(() => {
+    projetcs.forEach((project) => {
+    if (filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
+    } else {
+      project.classList.add('invisible');
+    }
+  });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+
+});
 
 // Utility Function
 function scrollIntoView(selector) {
